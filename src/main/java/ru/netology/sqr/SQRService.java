f160767c88281086700ea1ed9893818d73c1bcab
Диана Ильина <diana.1993.30@gmail.com>
2022-03-29 14:37:33 +0100

    Code fixed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,14 +4,9 @@ public class SQRService {
     public int calculate(int lower, int higher) {
         int counter = 0;
         for (int i = 10; i < 100; i++) {
-            int sqr_i = i * i;
-            if (sqr_i >= lower) {
-                if (sqr_i <= higher) {
-                    counter++;
-                } else {
-                    System.out.println("Количество квадратов в заданном диапазоне: " + counter);
-                    return counter;
-                }
+            int iSqr = i * i;
+            if (iSqr >= lower && iSqr <= higher) {
+                counter++;
             }
         }
         System.out.println("Количество квадратов в заданном диапазоне: " + counter);
